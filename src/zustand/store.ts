301a921e5ce79b47fbbf8766ea96448de4f6b145
Yarzadeh <hosein.yarzade@gmail.com>
2023-12-02 01:18:31 +0330

    add Action type

--- src/zustand/store.ts
+++ src/zustand/store.ts
@@ -1,18 +1,22 @@
+import { type } from 'os';
 import create from 'zustand';
 
 type Store = {
     count: number; //count type
+  
+  };
+  type Action ={
     increment: () => void; //first func type
     decrement: () => void; //second func type
     reset: () => void; //reset func type
-  };
+  }
   const initialState = {
     count: 0,
     
   }
 
   // create store
-  const useStore = create<Store>()((set) => ({
+  const useStore = create<Store & Action>()((set) => ({
     count: 0,
     increment: () => set((state) => ({ count: state.count + 1 })), //first func (increment)
     decrement: () => set((state) => ({ count: state.count - 1 })), //second func (decrement)
