d2d2464dab6e40dbb16e03f7ab8d87c73ca6f12e
Yarzadeh <hosein.yarzade@gmail.com>
2023-12-02 01:07:22 +0330

    add reset func

--- src/pages/index/App.tsx
+++ src/pages/index/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import useStore from './../../zustand/store';
 
 function App() {
-  const { count, increment, decrement } = useStore();
+  const { count, increment, decrement ,reset} = useStore();
   return (
     <div className="App">
       <button onClick={decrement}>-</button>
       <span>{count}</span>
       <button onClick={increment}>+</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
--- src/zustand/store.ts
+++ src/zustand/store.ts
@@ -4,13 +4,19 @@ type Store = {
     count: number; //count type
     increment: () => void; //first func type
     decrement: () => void; //second func type
+    reset: () => void; //reset func type
   };
+  const initialState = {
+    count: 0,
+    
+  }
 
   // create store
   const useStore = create<Store>()((set) => ({
     count: 0,
     increment: () => set((state) => ({ count: state.count + 1 })), //first func (increment)
     decrement: () => set((state) => ({ count: state.count - 1 })), //second func (decrement)
+    reset: () => set(initialState), //second func (decrement)
   }));
   
   export default useStore;
