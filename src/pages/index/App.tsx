0b6c7b00516aab6cf77512bee9a85bc6cbb00e2d
Yarzadeh <hosein.yarzade@gmail.com>
2023-12-04 00:19:32 +0330

    complate cleare persist storage

--- src/pages/index/App.tsx
+++ src/pages/index/App.tsx
@@ -6,20 +6,22 @@ import useStore from './../../zustand/persistStore';
 function App() {
   const { count, increment, decrement ,reset ,totalCount} = useStore();
   const clearStoeage = ()=>{
-    useStore.persist.clearStorage()
+    useStore.persist.clearStorage() // cleare persist storage
+    reset()
+    alert('clear storage done')
   }
   return (
     <Fragment>
     <div className="App">
-      <div>
-      <button onClick={decrement}>-</button>
-      <span>{count}</span>
-      <button onClick={increment}>+</button>
-      <button onClick={reset}>Reset</button>
-      <span>Total Action : {totalCount}</span>
+      <div className="Row">
+        <button onClick={decrement}>-</button>
+        <span>{count}</span>
+        <button onClick={increment}>+</button>
+        <button onClick={reset}>Reset</button>
+        <span>Total Action : {totalCount}</span>
       </div>
-      <div>
-      <button onClick={clearStoeage}>clear persist</button>
+      <div className="Row">
+        <button onClick={clearStoeage}>clear persist</button>
       </div>
       
     </div>
