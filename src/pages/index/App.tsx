4877803e858df1b086cb83904b93c4cd87a74487
Yarzadeh <hosein.yarzade@gmail.com>
2023-12-03 23:58:55 +0330

    add clearstorage

--- src/pages/index/App.tsx
+++ src/pages/index/App.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 // import useStore from './../../zustand/store';
 import useStore from './../../zustand/persistStore';
 
 function App() {
   const { count, increment, decrement ,reset ,totalCount} = useStore();
+  const clearStoeage = async ()=>{
+    await useStore.persist.rehydrate()
+  }
   return (
+    <Fragment>
     <div className="App">
+      <div>
       <button onClick={decrement}>-</button>
       <span>{count}</span>
       <button onClick={increment}>+</button>
       <button onClick={reset}>Reset</button>
       <span>Total Action : {totalCount}</span>
+      </div>
+      <div>
+      <button onClick={clearStoeage}>clear persist</button>
+      </div>
+      
     </div>
+    </Fragment>
   );
 }
 
